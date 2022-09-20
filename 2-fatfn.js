6c09de397fd629ddc009c34f930e050258940730
anlizfer <anlizfer@hotmail.com>
2022-09-20 06:42:07 -0500

    estudio javascript

--- 2-fatfn.js
+++ 2-fatfn.js
@@ -1,8 +1,12 @@
 /*
-    FUNCIONES - tambien lo podemos usar como si fuera clase, debido a que javascript es multiparadigma
+    functions:
+    FUNCIONES - tambien lo podemos usar como si fuera clase, debido a que javascript es multiparadigma, este implementa la herencia basada en prototipos.
+
 */
 function Fn(){
     return 'Angel Lizcano'
 }
 const r = Fn()
 console.log(r)
+
+//
