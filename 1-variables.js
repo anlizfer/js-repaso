407319f85d7dca8a972e0c0a63196c3e778694af
angel.lizcano.sie <angel.lizcano.sie@gmail.com>
2022-09-20 06:04:01 -0500

    estudio javascript

---
+++ 1-variables.js
@@ -0,0 +1,13 @@
+//el compilador de javascript, irà al comienzo del archivo y crea una variable con el nombre de la variable.
+//no se va usar
+console.log(miVariable)
+var miVariable ='variable'
+console.log(miVariable)
+
+//let es creado en la misma lìnea donde fue declaro y no como var que se crea al comienzo del archivo.
+let variable='variable creada con let'
+console.log(variable)
+
+//no podemos reasignar un valor
+const constante ='variable creada con const'
+console.log(constante)
---
+++ 2-fatfn.js
@@ -0,0 +1,8 @@
+/*
+    FUNCIONES - tambien lo podemos usar como si fuera clase, debido a que javascript es multiparadigma
+*/
+function Fn(){
+    return 'Angel Lizcano'
+}
+const r = Fn()
+console.log(r)
